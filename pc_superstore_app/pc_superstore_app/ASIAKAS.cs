49db79938ae51bc2e5140cc9c345273d0d9f8f3f
SaarinenVille <ville.saarinen@edu.keuda.fi>
2023-03-23 12:53:28 +0200

    asiakas, yhdistä classit, kirjautuminen

---
+++ pc_superstore_app/pc_superstore_app/ASIAKAS.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pc_superstore_app
+{
+    class ASIAKAS
+    {
+        YHDISTA yhteys = new YHDISTA();
+
+
+        public DataTable HaeAsiakkaat()
+        {
+            MySqlCommand haeKaikki = new MySqlCommand("SELECT kayttajatunnus, salasana FROM kayttajat", yhteys.otaYhteys());
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable taulukko = new DataTable();
+
+            adapter.SelectCommand = haeKaikki;
+            adapter.Fill(taulukko);
+
+            return taulukko;
+
+        }
+    }
+}
--- pc_superstore_app/pc_superstore_app/Form1.cs
+++ pc_superstore_app/pc_superstore_app/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,8 @@ namespace pc_superstore_app
 {
     public partial class Sisaankirjautuminen_form : Form
     {
+        YHDISTA kirjaudu = new YHDISTA();
+        ASIAKAS asiakas = new ASIAKAS();
         public Sisaankirjautuminen_form()
         {
             InitializeComponent();
@@ -27,6 +30,68 @@ namespace pc_superstore_app
         private void KirjauduBT_Click(object sender, EventArgs e)
         {
             
+            MySqlCommand asiakaskomento = new MySqlCommand("SELECT kayttajatunnus, salasana, oikeudet FROM kayttajat WHERE kayttajatunnus = @ktu AND salasana = @sal AND oikeudet = 2");
+            
+            asiakaskomento.Parameters.Add("@ktu", MySqlDbType.VarChar).Value = KayttajatunnusTB.Text;
+            asiakaskomento.Parameters.Add("@sal", MySqlDbType.VarChar).Value = SalasanaTB.Text;
+           
+
+
+            asiakaskomento.Connection = kirjaudu.otaYhteys();
+            
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            DataTable taulukko = new DataTable();
+
+            adapter.SelectCommand = asiakaskomento;
+            adapter.Fill(taulukko);
+
+            kirjaudu.avaaYhteys();
+
+            if (taulukko.Rows.Count > 0 ) 
+            {
+           
+                MessageBox.Show("Tervetuloa asiakas.");
+                // Avataan asiakkaille tarkoitettu etusivu
+            }
+            else
+            {
+                MessageBox.Show("Väärä salasana tai käyttäjätunnus.", "Virhe!");
+            }
+
+
+            /*MySqlCommand tyontekijakomento = new MySqlCommand("SELECT kayttajatunnus, salasana FROM kayttajat WHERE kayttajatunnus = @ktu AND salasana = @sal AND oikeudet = 1");
+
+            tyontekijakomento.Parameters.Add("@ktu", MySqlDbType.VarChar).Value = KayttajatunnusTB.Text;
+            tyontekijakomento.Parameters.Add("@sal", MySqlDbType.VarChar).Value = SalasanaTB.Text;
+
+            tyontekijakomento.Connection = kirjaudu.otaYhteys();
+
+
+            MySqlDataAdapter adapter2 = new MySqlDataAdapter();
+
+            DataTable taulukko2 = new DataTable();
+
+            adapter2.SelectCommand = tyontekijakomento;
+            adapter2.Fill(taulukko2);
+
+            kirjaudu.avaaYhteys();
+
+            if (taulukko.Rows.Count > 0)
+            {
+                MessageBox.Show("Tervetuloa työntekijä.");
+                // Avataan työntekijöille tarkoitettu etusivu
+            }
+            else
+            {
+                MessageBox.Show("Väärä salasana tai käyttäjätunnus.", "Virhe!");
+            }
+
+
+
+            //asiakas.HaeAsiakkaat();
+            //MessageBox.Show("Asiakkaiden haku onnistui");*/
         }
     }
 }
